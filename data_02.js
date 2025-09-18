19cd5747c6a5de7be9cfe7edfdd512a1ba2cc1d5
drz418 <72928662+drz418@users.noreply.github.com>
2025-09-18 16:59:03 -0400

    Add files via upload

---
+++ data_02.js
@@ -0,0 +1,38 @@
+const puzzles = [
+    {
+        solution: {
+            'STUDIO GHIBLI': ['Spirited Away', 'Princess Mononoke', 'Ponyo', 'The Tale of Princess Kaguya'],
+            'PSYCHOLOGICAL THRILLER': ['Perfect Blue', 'The Shinning', 'The Sixth Sense', 'Black Swan'],
+            'TINY PEOPLE': ['The Borrowers', 'Honey I Shrunk The Kids', 'The Indian and The Cupboard', 'Downsizing'],
+            'FEATURING: BEN AFFLECK': ['Good Will Hunting', 'Argo', 'Gigli', 'Gone Girl']
+        },
+        words: ['Spirited Away', 'Princess Mononoke', 'Ponyo', 'The Tale of Princess Kaguya',
+                'Perfect Blue', 'The Shinning', 'The Sixth Sense', 'Black Swan',
+                'Good Will Hunting', 'Argo', 'Gigli', 'Gone Girl',
+                'The Borrowers', 'Honey I Shrunk The Kids', 'The Indian In The Cupboard', 'Downsizing'
+            ],
+    },
+    // You can add more puzzles here in the future.
+];
+
+const wordImages = {
+    // This object is currently empty but will be useful if you decide to add images later.
+    'Spirited Away': 'images_02/spirited_away.jpg',
+    'Princess Mononoke': 'images_02/princess_mononoke.jpg',
+    'Ponyo': 'images_02/ponyo.jpg',
+    'The Tale of Princess Kaguya': 'images_02/the_tale_of_princess_kaguya.jpg',
+    'Perfect Blue': 'images_02/perfect_blue.jpg',
+    'The Shinning': 'images_02/the_shinning.jpg',
+    'The Sixth Sense': 'images_02/the_sixth_sense.jpg',
+    'Black Swan': 'images_02/black_swan.jpg',
+    'The Borrowers': 'images_02/the_borrowers.jpg',
+    'Honey I Shrunk The Kids': 'images_02/honey_i_shrunk_the_kids.jpg',
+    'The Indian In The Cupboard': 'images_02/the_indian_in_the_cupboard.jpg',
+    'Downsizing': 'images_02/downsizing.jpg',
+    'Good Will Hunting': 'images_02/good_will_hunting.jpg',
+    'Argo': 'images_02/argo.jpg',
+    'Gigli': 'images_02/gigli.jpg',
+    'Gone Girl': 'images_02/gone_girl.jpg'
+};
+
+export { puzzles, wordImages };
